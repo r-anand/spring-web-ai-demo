5fbc8f685c8e907f2245eecbf631fbbac2fcdae6
Anand Ramasubramanian <ramasubramanian.anand@gmail.com>
2024-07-14 18:33:57 +0530

    initial commit

---
+++ src/main/java/com/tech_bytes/projects/SpringaiApplication.java
@@ -0,0 +1,13 @@
+package com.tech_bytes.projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaiApplication.class, args);
+	}
+
+}
